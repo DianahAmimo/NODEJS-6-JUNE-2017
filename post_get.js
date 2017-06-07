f8b477adf201db122d578deedf34144ef4738013
dianne <dianneprinsescah@gmail.com>
2017-06-07 07:24:49 +0000

    Nodejs commit

--- post_get.js
+++ post_get.js
@@ -1,69 +1,15 @@
-// var http = require('http');
 
-// var request = http.request({
-//   host: 'example.com',
-//   path: '/endpoint',
-//   port: 443,
-//   method: 'POST',
-//   headers: {
-//     'Authorization': clientId + ':' + signature(endpoint, postBody), 
-//     'Content-Type': 'application/json',
-//     'Content-Length': postBody.length
-//   }
-// }, function(response) {
-//   var reply = '';
-//   response.on('data', function(content) {
-//     reply += content;
-//   });
+var blog = require("fs");
 
-//   response.on('end', function() {
-//     return cb(reply);
-//   });
-// });
-
-// request.write(postBody);
-// request.end();
-
-// var fs = require("fs");
-
-// // Asynchronous - Opening File
-// console.log("Going to open file!");
-// fs.open('DAILY BLOG#1 .docx', 'r+', function(err, fd) {
-//    if (err) {
-//       return console.error(err);
-//    }
-//   console.log("File opened successfully!");     
-// });
-
-
-var http = require("http"); 
-http.createServer(function (request, response) {
-  fs.readFile("'DAILY BLOG#1 .docx'", function(err, data){
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write(data);
-  response.end();
-});
-// Send the HTTP header
-// HTTP Status: 200 : OK
-// Content Type: text/plain
-response.writeHead(200, {'Content-Type': 'text/plain'});
-// Send the response body as "Hello World"
-response.end('Hello World, I Love NODE!\n'); }).listen(8081);
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
-
-
-// var fs = require("fs");
-
-// console.log("Going to get file info!");
-// fs.open('DAILY BLOG#1 .docx', 'r+', function(err, fd) {
-//    if (err) {
-//        return console.error(err);
-//    }
-//    // console.log(stats);
-//    console.log("Got file info successfully!");
+console.log("Going to get file info!");
+blog.stat('DAILY BLOG#1 .docx', function (err, stats) {
+   if (err) {
+       return console.error(err);
+   }
+   console.log(stats);
+   console.log("Got file info successfully!");
    
    // Check file type
-   // console.log("isFile ? " + stats.isFile());
-   // console.log("isDirectory ? " + stats.isDirectory());    
-// });
+   console.log("isFile ? " + stats.isFile());
+   console.log("isDirectory ? " + stats.isDirectory());    
+});
